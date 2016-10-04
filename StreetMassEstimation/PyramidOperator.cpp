e9e96e1668cfb9b342512893ac2eada22645d194
gnishida <gnishida@purdue.edu>
2016-10-04 18:50:40 -0400

    align indent

--- StreetMassEstimation/PyramidOperator.cpp
+++ StreetMassEstimation/PyramidOperator.cpp
@@ -1,17 +1,12 @@
 #include "PyramidOperator.h"
 #include "CGA.h"
 #include "Shape.h"
-#include "Utils.h"
 
 namespace cga {
 
 	PyramidOperator::PyramidOperator(const std::string& height) {
 		this->name = "pyramid";
 		this->height = height;
-
-		if (!utils::isNumber(height)) {
-			this->params.push_back(height);
-		}
 	}
 
 	boost::shared_ptr<Shape> PyramidOperator::apply(boost::shared_ptr<Shape>& shape, const Grammar& grammar, std::list<boost::shared_ptr<Shape> >& stack) {
